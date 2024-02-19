552a5886b7a3fd5798d5c0488851e683c84ffb1c
MohamedSaneeth <saneeth@titumlk.onmicrosoft.com>
2024-02-19 12:05:17 +0530

    Event hadling

--- abc_car_traders/CustomerMenu.Designer.cs
+++ abc_car_traders/CustomerMenu.Designer.cs
@@ -49,6 +49,7 @@
             this.Controls.Add(this.label1);
             this.Name = "CustomerMenu";
             this.Text = "CustomerMenu";
+            this.Load += new System.EventHandler(this.CustomerMenu_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
